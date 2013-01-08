ff5c4d3a58f5a4cc50d3cea7113353903e827286
Sam Wilson <sam@samwilson.id.au>
2013-01-08 16:16:51 +0800

    Some modifications to ease release creation.

---
+++ tag-and-release.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ -z $1 ]; then
+	echo "Usage: $0 <tag_name>"
+	exit 1
+fi
+
+TAG=$1
+echo "Creating Git tag: $TAG"
+git tag -a "$TAG"
+
+FILENAME="webdb_$TAG.tgz"
+echo "Creating release file: $FILENAME"
+tar -czf $FILENAME \
+  --exclude-vcs \
+  --exclude-from='.gitignore' \
+  --exclude='nbproject' \
+  --exclude='*.sh' \
+  --exclude='*.tar' \
+  --exclude='*.tgz' \
+  *
+
