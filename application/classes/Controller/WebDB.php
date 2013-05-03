4cbe4e1d7256223653feee54663bc9e53f9719b5
Sam Wilson <sam@samwilson.id.au>
2013-05-03 21:20:44 +0800

    Case fix.

--- application/classes/Controller/WebDB.php
+++ application/classes/Controller/WebDB.php
@@ -463,7 +463,7 @@ class Controller_WebDB extends Controller_Template
 		try
 		{
 			$hash = $this->request->param('id', FALSE);
-			$this->view->file = new Webdb_File_CSV($hash);
+			$this->view->file = new WebDB_File_CSV($hash);
 		} catch (Kohana_Exception $e)
 		{
 			$this->add_template_message($e->getMessage());
