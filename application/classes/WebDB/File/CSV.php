c89b052458c82ee2223cba94b7da97f8cbccf3ad
Sam Wilson <sam@samwilson.id.au>
2014-03-24 10:09:39 +0800

    Correct date checking.

--- application/classes/WebDB/File/CSV.php
+++ application/classes/WebDB/File/CSV.php
@@ -187,7 +187,7 @@ class Webdb_File_CSV
 					if ($err) $col_errors[] = $err;
 				}
 				// Dates
-				if ($column->get_type()=='date' && !empty($value) && preg_match('/\n{4}-\n{2}-\n{2}/', $value)!==1)
+				if ($column->get_type()=='date' && !empty($value) && preg_match('/\d{4}-\d{2}-\d{2}/', $value)!==1)
 				{
 					$col_errors[] = 'Value ('.$value.') not in date format';
 				}
