2f8775a5ac2166c722e819bcd3b53a45dd6dc289
Sam Wilson <sam@samwilson.id.au>
2015-01-28 10:23:49 +1100

    Full cache config, and IIS rewriting.

--- application/config/cache.php
+++ application/config/cache.php
@@ -3,5 +3,12 @@ return array
 (
 	'file'    => array(
 		'driver'             => 'file',
+		'cache_dir'          => APPPATH.'cache',
+		'default_expire'     => 3600,
+		'ignore_on_delete'   => array(
+			'.gitignore',
+			'.git',
+			'.svn'
+		)
 	)
 );
